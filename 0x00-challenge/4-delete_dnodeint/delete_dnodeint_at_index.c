e6a56b39f869d7ef2ebbc2b169fa693e460fe68e
MuhabGamal <muhabme@gmail.com>
2024-03-21 03:24:03 +0200

    fix dnodeint

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -16,9 +16,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int p;
 
 	if (*head == NULL)
-	{
 		return (-1);
-	}
+
 	saved_head = *head;
 	p = 0;
 	while (p < index && *head != NULL)
@@ -35,18 +34,16 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		tmp = (*head)->next;
 		free(*head);
-		*head = tmp;
 		if (tmp != NULL)
-		{
 			tmp->prev = NULL;
-		}
+		*head = tmp;
 	}
 	else
 	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
+		(*head)->prev->next = (*head)->next;
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
+		free(*head);
 		*head = saved_head;
 	}
 	return (1);
